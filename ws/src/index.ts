ad75476ad55807e0d8ee03948379d3943bc604cd
Ayushmaan Pandey <ayushmaan1122pandey@gmail.com>
2025-08-05 00:46:34 +0530

    running application

--- ws/src/index.ts
+++ ws/src/index.ts
@@ -2,6 +2,7 @@ import { WebSocket, WebSocketServer } from "ws";
 import { UserManager } from "./UserManager";
 
 const wss = new WebSocketServer({ port: 8080 });
+console.log(`ws running at port : 8080`);
 
 wss.on("connection", (ws: WebSocket) => {
   console.log("user connected", ws);
